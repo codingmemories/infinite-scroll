8f97ba44e217fe5c75a63d1a3021d89284f609d9
Vasylyna Lykhogodenko <vasilisapj89@gmail.com>
2024-02-28 15:50:09 +0200

    add fetch api

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+// Unsplash API
+const count = 10;
+const apiKey = 'Wife9PKv3hOM5jY74_kFGK7zuDT_RGWgBg5NZFFsHLs';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get photos from Unsplash API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch(error) {
+        // Catch Error Here
+    }
+}
+
+// on Load
+getPhotos();
